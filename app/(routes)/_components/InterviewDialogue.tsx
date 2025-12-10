8305e7bbff40286688a18a3d6fe57dccaea51224
shrilakshmi-24 <shrilakshmishenoy24@gmail.com>
2025-12-10 16:50:53 +0530

    fix: move Tabs component outside of DialogDescription for better structure

--- app/(routes)/_components/InterviewDialogue.tsx
+++ app/(routes)/_components/InterviewDialogue.tsx
@@ -16,30 +16,37 @@ import JobDescription from "./JobDescription";
 function InterviewDialogue() {
   return (
     <Dialog>
-      <DialogTrigger>
+      <DialogTrigger asChild>
         <Button size={"lg"}>+ Create Interview</Button>
       </DialogTrigger>
+
       <DialogContent className="min-w-3xl">
         <DialogHeader>
           <DialogTitle>Please select the relevant one</DialogTitle>
           <DialogDescription>
-            <Tabs defaultValue="upload-resume" className="w-full mt-5">
-              <TabsList>
-                <TabsTrigger value="upload-resume">Upload Resume</TabsTrigger>
-                <TabsTrigger value="job-description">Job Description</TabsTrigger>
-              </TabsList>
-              <TabsContent value="upload-resume">
-                <ResumeUpload />
-              </TabsContent>
-              <TabsContent value="job-description">
-                <JobDescription />
-              </TabsContent>
-            </Tabs>
+            Select either to upload a resume or provide a job description.
           </DialogDescription>
         </DialogHeader>
+
+        {/* Tabs moved outside of DialogDescription */}
+        <Tabs defaultValue="upload-resume" className="w-full mt-5">
+          <TabsList>
+            <TabsTrigger value="upload-resume">Upload Resume</TabsTrigger>
+            <TabsTrigger value="job-description">Job Description</TabsTrigger>
+          </TabsList>
+
+          <TabsContent value="upload-resume">
+            <ResumeUpload />
+          </TabsContent>
+
+          <TabsContent value="job-description">
+            <JobDescription />
+          </TabsContent>
+        </Tabs>
+
         <DialogFooter className="flex gap-6">
-            <Button variant="secondary">Cancel</Button>
-            <Button>Proceed</Button>
+          <Button variant="secondary">Cancel</Button>
+          <Button>Proceed</Button>
         </DialogFooter>
       </DialogContent>
     </Dialog>
